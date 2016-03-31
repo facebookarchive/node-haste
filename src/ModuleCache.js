11984b87b600b5b62f2be073eedd58bd9cf2fbde
cpojer <christoph.pojer@gmail.com>
2016-03-31 13:31:47 -0700

    Use ES2015 WeakMap in ModuleCache.

--- src/ModuleCache.js
+++ src/ModuleCache.js
@@ -24,6 +24,7 @@ class ModuleCache {
     this._transformCode = transformCode;
     this._depGraphHelpers = depGraphHelpers;
     this._assetDependencies = assetDependencies;
+    this._packageModuleMap = new WeakMap();
 
     fastfs.on('change', this._processFileChange.bind(this));
   }
@@ -72,22 +73,21 @@ class ModuleCache {
   }
 
   getPackageForModule(module) {
-    // TODO(amasad): use ES6 Map.
-    if (module.__package) {
-      if (this._packageCache[module.__package]) {
-        return this._packageCache[module.__package];
+    if (this._packageModuleMap.has(module)) {
+      const packagePath = this._packageModuleMap.get(module);
+      if (this._packageCache[packagePath]) {
+        return this._packageCache[packagePath];
       } else {
-        delete module.__package;
+        this._packageModuleMap.delete(module);
       }
     }
 
     const packagePath = this._fastfs.closest(module.path, 'package.json');
-
     if (!packagePath) {
       return null;
     }
 
-    module.__package = packagePath;
+    this._packageModuleMap.set(module, packagePath);
     return this.getPackage(packagePath);
   }
 
